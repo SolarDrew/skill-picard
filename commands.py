09126a8a1435057cf03fd12d6b88797ab174d9da
Stuart Mumford <stuart@cadair.com>
2019-07-23 19:59:22 +0100

    Make announce new rooms work

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from textwrap import dedent
 
@@ -76,56 +77,56 @@ class PicardCommands:
     @match_regex('!createroom (?P<name>.+?) "(?P<topic>.+?)"')
     @ignore_appservice_users
     async def on_create_room_command(self, message):
-        # TODO: Ignore duplicates here, if a slack user sends this message in a
-        # bridged room, we react to both the original slack message and the
-        # matrix message.
+        await message.respond('Creating room please wait, this takes a little while...')
+
+        name, topic = (message.regex['name'],
+                       message.regex['topic'])
+
+        is_public = self.config.get("make_public", False)
+        matrix_room_id = await self.create_new_matrix_room()
+
+        await self.configure_new_matrix_room_pre_bridge(matrix_room_id, is_public)
+
         async with self._slack_channel_lock:
-            await message.respond('Creating room please wait...')
-
-            name, topic = (message.regex['name'],
-                           message.regex['topic'])
-
-            is_public = self.config.get("make_public", False)
-            matrix_room_id = await self.create_new_matrix_room()
-
-            await self.configure_new_matrix_room_pre_bridge(matrix_room_id, is_public)
-
             # Create the corresponding slack channel
             slack_channel_id = await self.create_slack_channel(name)
 
-            # Link the two rooms
-            await self.link_room(matrix_room_id, slack_channel_id)
+            # Just to make sure we get the slack new room event
+            await asyncio.sleep(0.1)
 
-            # Setup the matrix room
-            matrix_room_alias = await self.configure_new_matrix_room_post_bridge(
-                matrix_room_id, name, topic)
+        # Link the two rooms
+        await self.link_room(matrix_room_id, slack_channel_id)
 
-            # Set the description of the slack channel
-            await self.set_slack_channel_description(slack_channel_id, topic)
+        # Setup the matrix room
+        matrix_room_alias = await self.configure_new_matrix_room_post_bridge(
+            matrix_room_id, name, topic)
 
-            # Invite Command User
-            if message.connector is self.matrix_connector:
-                user = message.raw_event['sender']
-                target = matrix_room_id
-                command_room = message.target
+        # Set the description of the slack channel
+        await self.set_slack_channel_description(slack_channel_id, topic)
 
-                await self.opsdroid.send(UserInvite(target=target,
-                                                    user=user,
-                                                    connector=message.connector))
+        # Invite Command User
+        if message.connector is self.matrix_connector:
+            user = message.raw_event['sender']
+            target = matrix_room_id
+            command_room = message.target
 
-            elif message.connector is self.slack_connector:
-                user = message.raw_event['user']
-                target = slack_channel_id
-                command_room = await self.matrix_room_id_from_slack_channel_name(message.target)
+            await self.opsdroid.send(UserInvite(target=target,
+                                                user=user,
+                                                connector=message.connector))
 
-                await self.invite_user_to_slack_channel(slack_channel_id, user)
+        elif message.connector is self.slack_connector:
+            user = message.raw_event['user']
+            target = slack_channel_id
+            command_room = await self.matrix_room_id_from_slack_channel_name(message.target)
 
-            # Inform users about the new room/channel
-            pill = f'<a href="https://matrix.to/#/{matrix_room_alias}">{matrix_room_alias}</a>'
-            await self.opsdroid.send(Message(f"Created a new room: {pill}",
-                                             target=command_room,
-                                             connector=self.matrix_connector))
+            await self.invite_user_to_slack_channel(slack_channel_id, user)
 
-            await self.announce_new_room(matrix_room_id, slack_channel_id)
+        # Inform users about the new room/channel
+        pill = f'<a href="https://matrix.to/#/{matrix_room_alias}">{matrix_room_alias}</a>'
+        await self.opsdroid.send(Message(f"Created a new room: {pill}",
+                                         target=command_room,
+                                         connector=self.matrix_connector))
 
-            return matrix_room_id
+        await self.announce_new_room(matrix_room_alias, message.user, topic)
+
+        return matrix_room_id
